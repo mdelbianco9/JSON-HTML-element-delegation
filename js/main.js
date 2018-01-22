9c9b9dfcc729fda75cf6bf00b79bce376b45c938
marcus delbianco <mdelbianco9@gmail.com>
2018-01-22 15:17:17 -0800

    Function that uses JSON to input data to HTML upon click of a tags

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+
+// // var app5 = new Vue({
+// //   el: '#app-5',
+// //   data: {
+// //     message: 'Hello Vue.js!'
+// //   },
+// //   methods: {
+// //     reverseMessage: function () {
+// //       this.message = this.message.split('').reverse().join('')
+// //     }
+// //   }
+// // })
+
+// var root = 'https://jsonplaceholder.typicode.com';
+
+// $.ajax({
+//   url: root + '/photos/1',
+//   method: 'GET'
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+// function bookfinder() {
+//   $('#results').empty();
+//   var search = document.getElementById('book-search').value;
+
+//   $.ajax ({
+//     url: "https://jsonplaceholder.typicode.com" + search,
+//     datatype: "json",
+
+//     success: function(data) {
+//       for (i = 0; i < data.items.length; i++) {
+//         var mydata = data.items[i].volumeInfo;
+
+//       }
+//     },
+
+//     type: "GET"
+//   });
+
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
